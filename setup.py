3d1ae5f5e31ab2a69564f23a94909fbe98036287
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-04-08 18:31:50 +0200

    changed version number to 0.2

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 
 # setup
 setup(name='pyobs_pilar',
-      version='0.1',
+      version='0.2',
       description='pyobs component for the Pilar TCS',
       packages=['pyobs_pilar'])
